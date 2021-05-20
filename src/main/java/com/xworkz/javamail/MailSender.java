762f6f72678818a7431b995f8bc4de68bbb208b1
Rakshitha86 <rakshithar.xworkz@gmail.com>
2021-05-20 21:26:10 +0530

    Mail

---
+++ src/main/java/com/xworkz/javamail/MailSender.java
@@ -0,0 +1,40 @@
+package com.xworkz.javamail;
+
+import java.util.Properties;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+public class MailSender {
+
+	public static void main(String[] args) {
+
+		JavaMailSenderImpl javaMailSenderImpl = new JavaMailSenderImpl();
+		javaMailSenderImpl.setHost("smtp.gmail.com");
+		javaMailSenderImpl.setPort(587);
+		javaMailSenderImpl.setUsername("rakshithar.xworkz@gmail.com");
+		javaMailSenderImpl.setPassword("Xworkz@1234");
+
+		Properties javaMailproperties = new Properties();
+		javaMailproperties.put("mail.smtp.starttls.enable", "true");
+		javaMailproperties.put("mail.smtp.auth", "true");
+		javaMailproperties.put("mail.transport.protocol", "smtp");
+		javaMailproperties.put("mail.debug", "true");
+		javaMailproperties.put("mail.smtp.ssl.trust", "*");
+
+		javaMailSenderImpl.setJavaMailProperties(javaMailproperties);
+
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		String[] bccs = { "preranam.xworkz@gmail.com", "varsha.xworkz@gmail.com" };
+		mailMessage.setFrom("rakshithar.xworkz@gmail.com");
+		mailMessage.setTo("varalakshmi.xworkz@gmail.com");
+		mailMessage.setCc("rakshitharajendra99@gmail.com");
+		mailMessage.setBcc(bccs);
+		mailMessage.setSubject("Demo Java Mail Sender");
+		mailMessage.setText("Hi \n Hello \n How are you");
+
+		javaMailSenderImpl.send(mailMessage);
+
+	}
+}
